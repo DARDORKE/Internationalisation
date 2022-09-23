48d4c72cb6e85d323e6bd89e63adec7f067d9e95
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-23 15:44:49 +0200

    Mise en application d'internationalisation 1/2

--- src/Controller/NavbarController.php
+++ src/Controller/NavbarController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class NavbarController extends AbstractController
 {
     #[Route('/navbar', name: 'app_navbar')]
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
+        
+
         return $this->render('navbar/index.html.twig', [
             'controller_name' => 'NavbarController',
         ]);
